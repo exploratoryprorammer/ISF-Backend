1621ae2deaaafcbcc407e4bc62171d85f5811132
exploratoryprorammer <rohan.k.sah@gmail.com>
2025-09-17 23:51:25 -0500

    feat: added mongodb driver

---
+++ db/mongo.go
@@ -0,0 +1,41 @@
+package db
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"time"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/options"
+)
+
+var Client *mongo.Client
+
+func ConnectDB() *mongo.Client {
+	uri := "mongo://localhost:27017"
+
+	client, err := mongo.Connect(options.Client().ApplyURI(uri))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	
+	err = client.Connect(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = client.Ping(ctx, nil)
+	if err != nil {
+		log.Fatal("Could not connect to MongoDB", err)
+	} else {
+		fmt.Println("Connected to MongoDB")
+	}
+
+	Client = client
+
+
+}
